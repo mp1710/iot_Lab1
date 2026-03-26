0e0ef5cd23f553b40ba38ac29c85865fd8779c53
nacho2106 <inegri2106@gmail.com>
2026-03-26 20:27:40 -0300

    Vocales y consonantes

---
+++ global.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+//Número de vocales en una cadena
+int vocales(char *string) {
+    int num_vocales = 0;
+    char *p = string; // puntero para recorrer la cadena
+    while (*p != '\0') {
+        if (*p == 'a' || *p == 'e' || *p == 'i' || *p == 'o' || *p == 'u' ||
+            *p == 'A' || *p == 'E' || *p == 'I' || *p == 'O' || *p == 'U') {
+            num_vocales++;
+        }
+        p++;
+    }
+    return num_vocales;
+}
+
+//Número de consonantes en una cadena
+int consonantes(char *string) {
+    int num_consonantes = 0;
+    char *p = string; // puntero para recorrer la cadena
+    while (*p != '\0') {
+        if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z')) {
+            if (*p != 'a' && *p != 'e' && *p != 'i' && *p != 'o' && *p != 'u' &&
+                *p != 'A' && *p != 'E' && *p != 'I' && *p != 'O' && *p != 'U') {
+                num_consonantes++;
+            }
+        }
+        p++;
+    }
+    return num_consonantes;
+}
