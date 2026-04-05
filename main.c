82598edb5462dd68ee64fa5c89d2560d707eb6d2
nacho2106 <inegri2106@gmail.com>
2026-04-05 16:57:17 -0300

    Refactor function comments and add tests

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 
 int main(void) {
 
-// FUNCIÓN 1
+// FUNCIÓN 1 init_lab
     printf("Testing init_lab:\n");
     init_lab();
     printf("\n");
-// FUNCIÓN 2
+// FUNCIÓN 2 eq_solver
     printf("Testing eq_solver:\n");
     // Creo coeficientes para la ecuacion x^2 - 5x + 6 = 0, que tiene raices reales y distintas (2 y 3)
     coeff_t coef = {1, -5, 6};
@@ -24,7 +24,7 @@ int main(void) {
         free(result);
         printf("\n");
     }
-// FUNCIÓN 3
+// FUNCIÓN 3 string_copy
     printf("Testing string_copy:\n");
     char source[] = "Prueba de string copy";
     char destination[20];
@@ -33,14 +33,14 @@ int main(void) {
     printf("Destination: %s\n", destination);
     printf("\n");
 
-// FUNCIÓN 4
+// FUNCIÓN 4 max_index
     printf("Testing max_index:\n");
     int32_t arr_int[] = {1, 3, 5, 2, 4};
     size_t size_int = sizeof(arr_int) / sizeof(arr_int[0]);
     max_index(arr_int, sizeof(int32_t), size_int);
     printf("\n");
 
-// FUNCIÓN 5
+// FUNCIÓN 5 min_index
     printf("Testing min_index:\n");
     int32_t arr_int[] = {5, 1, 9, 2, 5};
     float arr_float[] = {3.5, 2.1, 4.6, 1.1, 3.3};
@@ -52,7 +52,7 @@ int main(void) {
     printf("Para array de doubles:\n");
     min_index(arr_double, sizeof(double), sizeof(arr_double) / sizeof(arr_double[0]));
 
-// FUNCIÓN 6
+// FUNCIÓN 6 swap
     printf("Testing swap:\n");
     int32_t a = 5, b = 10;
     swap(&a, &b, sizeof(int32_t));
@@ -64,18 +64,28 @@ int main(void) {
     swap(&m, &n, sizeof(double));
     printf("Después del swap: m = %.2lf, n = %.2lf\n", m, n);
 
-// FUNCIÓN 7
+// FUNCIÓN 7 print_reverse_string
+    printf("Testing print_reverse_string:\n");
     char str[] = "Hola, buenas tardes";
     print_reverse_string(str, sizeof(char), strlen(str));
+    
     
 
 // FALTA DAYS_LEFT
 
+
     
-// FUNCIÓN 9
+// FUNCIÓN 9 vocales
+    printf("Testing vocales:\n");
     char string[] = "Ejemplo para contar vocales";
     int num_vocales = vocales(string);
     printf("Número de vocales en la cadena: %d\n", num_vocales);
+
+// FUNCIÓN 10 consonantes
+    printf("Testing consonantes:\n");
+    char string[] = "Ejemplo para contar consonantes";
+    int num_consonantes = consonantes(string);
+    printf("Número de consonantes en la cadena: %d\n", num_consonantes);
     
     return 0;
 }
