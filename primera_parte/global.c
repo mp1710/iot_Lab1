44be25ab5e3015c55fbe46492be5bb6619ce7f85
pilarmartinez <martinezmp.2021@gmail.com>
2026-04-06 11:35:00 -0300

    Update global.c

--- primera_parte/global.c
+++ primera_parte/global.c
@@ -276,12 +276,12 @@ matriz_t matrix_sub(matriz_t A, matriz_t B) {
     return C;
 }
 
-int32_L string_length(char *string) {
+int32_t string_length(char *string) {
     if (string == NULL) {
         return -1; // Retorna -1 si el puntero es NULL
     }
 
-    int32_L length = 0;
+    int32_t length = 0;
 
     while (string[length] != '\0' && length <= 20) { // Límite arbitrario de 20 caracteres para evitar desbordamiento
         if (length >= 20) {
@@ -293,11 +293,11 @@ int32_L string_length(char *string) {
     return length;
 }
 
-int32_L string_words(char *string) {
+int32_t string_words(char *string) {
     if (string == NULL) return -1;
 
-    int32_L words = 0;
-    int32_L length = 0;
+    int32_t words = 0;
+    int32_t length = 0;
     int en_word = 0;
 
     while (length < 50 && string[length] != '\0') {
