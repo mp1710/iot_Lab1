9087defed4a5643e0029afc2402bd5fe414f9a24
pilarmartinez <martinezmp.2021@gmail.com>
2026-04-05 23:55:14 -0300

    Add files via upload

--- estudiantes.h
+++ estudiantes.h
@@ -3,40 +3,42 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef struct {
+
+typedef struct estudiantes{
 
     char nombre[64];
     char apellido[64];
     uint32_t ci;
     uint8_t grado;
     float promedio;
-   
+    struct estudiantes *siguiente;
+
 } estudiante_t;
 
-typedef struct nodo{
-    estudiante_t dato;
-    struct nodo *siguiente;
-} nodo_t;
+//La variabale cabeza es un puntero a un puntero a estudiante_t
 
-// Funciones a implementar? Puede faltar la de ordenar por ci/nombre
+//FUNCION 1
+void agregar_estudiante(estudiante_t **cabeza, char nombre[], char apellido[], uint32_t ci, int grado, float promedio);
 
-//agregar_estudiante
-void agregar_estudiante(nodo_t **cabeza, estudiante_t nuevo_estudiante);
-//mostrar_lista
-void mostrar_lista(nodo_t *cabeza);
-//mostrar_estudiante
-void mostrar_estudiante(estudiante_t estudiante);
-//eliminar_estudiante
-void eliminar_estudiante(nodo_t **cabeza, uint32_t ci);
-//buscar_estudiante_nombre
-estudiante_t* buscar_estudiante_nombre(nodo_t *cabeza, const char *nombre);
-//buscar_estudiante_apellido
-estudiante_t* buscar_estudiante_apellido(nodo_t *cabeza, const char *apellido);
-//buscar_estudiante_ci
-estudiante_t* buscar_estudiante_ci(nodo_t *cabeza, uint32_t ci);
-//calcular_calificacion
-float calificacion(estudiante_t estudiante);
-//liberar_lista
-void liberar_lista(nodo_t **cabeza);
+//FUNCION 2
+void eliminar_estudiante(estudiante_t **cabeza, uint32_t ci);
+
+//FUNCION 3
+void mostrar_lista(estudiante_t *cabeza);
+
+//FUNCION 4
+void mostrar_estudiante_nombre(estudiante_t *cabeza, const char *nombre);
+
+//FUNCION 5
+void mostrar_estudiante_ci(estudiante_t *cabeza, uint32_t ci);
+
+//FUNCION 6
+void ordenar_por_apellido(estudiante_t *cabeza);
+
+//FUNCION 7
+void ordenar_por_ci(estudiante_t *cabeza);
+
+//FUNCION 8
+void calificacion(estudiante_t *estudiante);
 
 #endif // ESTUDIANTES_H
